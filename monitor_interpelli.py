4339212ff81d56165819bcdcad19ab27282dc718
diavolerie <nocecris@gmail.com>
2026-08-17 12:53:19 +0200

    Update monitor_interpelli.py

--- monitor_interpelli.py
+++ monitor_interpelli.py
@@ -78,6 +78,8 @@ def extract_items(site_name, base_url, html_text, keywords):
     return list(dedup.values())
 
 def main():
+    send_telegram("Test Telegram OK")
+
     session = requests.Session()
     session.headers.update({
         "User-Agent": "Mozilla/5.0",
